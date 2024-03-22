77920dacfbdb9e14d29177566e0e7ea64f279cd3
Dave-deJong <dndj81@yahoo.co.uk>
2024-03-22 14:41:17 +0000

    hourly forcasts but on update multiple entries

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ async function getAndDisplayWeather() {
   const weather = await retrieveWeather();
   displayWeather(weather);
 }
+// function to change the units
 function changeUnits() {
   if (unitsMetric) {
     url =
@@ -22,6 +23,37 @@ function changeUnits() {
     unitsMetric = true;
   }
 }
+//display hourly weather
+function displayHourlyWeather(weather) {
+  const hourlySection = document.getElementById("hourlySection");
+  let j = 0;
+  for (let i = 0; i < weather.hourly.time.length; i++) {
+    if (weather.hourly.time[i] > weather.current.time) {
+      // const hourName = `hour${i}Details`;
+      const newHourly = document.createElement("div");
+      const newHourTime = document.createElement("p");
+      const newHourTemp = document.createElement("p");
+      const newHourType = document.createElement("p");
+      const newHourRain = document.createElement("p");
+      hourlySection.appendChild(newHourly);
+      newHourly.appendChild(newHourTime);
+      newHourly.appendChild(newHourTemp);
+      newHourly.appendChild(newHourType);
+      newHourly.appendChild(newHourRain);
+      newHourTime.textContent = weather.hourly.time[i]
+        .toString()
+        .substring(11, 16);
+      newHourTemp.textContent = `${weather.hourly.temperature_2m[i]} ${weather.current_units.temperature_2m}`;
+      newHourType.textContent = weather.hourly.weather_code[i];
+      newHourRain.textContent = weather.hourly.precipitation_probability[i];
+      j++;
+      if (j > 4) {
+        return;
+      }
+    }
+  }
+}
+
 // write a function to fetch the info from an API
 async function retrieveWeather() {
   const response = await fetch(url);
@@ -41,6 +73,8 @@ function displayWeather(weather) {
   currentTemp.textContent = `Current Temperature:${weather.current.temperature_2m} ${weather.current_units.temperature_2m}`;
   const maxMinTemp = document.getElementById("currentMaxMin");
   maxMinTemp.textContent = `Max Temp: ${weather.daily.temperature_2m_max[0]} ${weather.current_units.temperature_2m} Min Temp: ${weather.daily.temperature_2m_min[0]} ${weather.current_units.temperature_2m} `;
+  const windSpeed = document.getElementById("windSpeed");
+  windSpeed.textContent = `Wind speed : ${weather.current.wind_speed_10m} ${weather.current_units.wind_speed_10m}`;
   const sunrise = document.getElementById("sunrise");
   sunrise.textContent = `Sunrise : ${weather.daily.sunrise[0]
     .toString()
@@ -49,6 +83,7 @@ function displayWeather(weather) {
   sunset.textContent = `Sunset : ${weather.daily.sunset[0]
     .toString()
     .substring(11, 16)}`;
+  displayHourlyWeather(weather);
 }
 // place event listeners on the button
 const updateButton = document.getElementById("updateButton");
